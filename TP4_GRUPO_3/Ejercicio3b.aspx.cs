d26281480a9059d176a2b45ed6681ff161d6b9d1
ValentinSel <166621061+ValentinSel@users.noreply.github.com>
2024-04-29 18:18:53 -0300

    Ejercicio 3b - GridView
    Agregué la carga de datos al GridView

--- TP4_GRUPO_3/Ejercicio3b.aspx.cs
+++ TP4_GRUPO_3/Ejercicio3b.aspx.cs
@@ -12,9 +12,27 @@ namespace TP4_GRUPO_3
 {
     public partial class Ejercicio3b : System.Web.UI.Page
     {
+        private const string stringConnection = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Libreria;Integrated Security=True";
+        private string consultaLibros = "SELECT * FROM Libros WHERE IdTema = ";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
+                consultaLibros += Request.QueryString["temaId"];
 
+                SqlConnection connection = new SqlConnection(stringConnection);
+                connection.Open();
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(consultaLibros, connection);
+
+                DataSet dataSet = new DataSet();
+                sqlDataAdapter.Fill(dataSet, "TablaLibros");
+
+                GVLibros.DataSource = dataSet.Tables["TablaLibros"];
+                GVLibros.DataBind();
+
+                connection.Close();
+            }
         }
         protected void LblVolverAtras_Click(object sender, EventArgs e)
         {
